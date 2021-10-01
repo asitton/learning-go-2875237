5277687319876a3acf458dc8c519ce4195516daa
alex.sitton <alex.sitton@maine.edu>
2021-10-01 16:37:39 -0400

    working with dates and times

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	n := time.Now()
+	fmt.Println("I recorded this video at ", n)
+
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Println("Go was launched at ", t)
+	fmt.Println(t.Format(time.ANSIC))
 
 }
