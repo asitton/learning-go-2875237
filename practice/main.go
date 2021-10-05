6f4417b5ca3eeb7a5b1850c565d772da3043ae2b
alex.sitton <alex.sitton@maine.edu>
2021-10-05 19:11:39 -0400

    ordered values in arrays

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors[0])
+
+	var number = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(number)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers", len(number))
 }
